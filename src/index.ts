a09d753585b8cd353c42ddddb861c0be83745dcb
Devansh Sharma <kdevanshsharma23@gmail.com>
2020-09-22 13:15:03 +0530

    fix: count

--- src/index.ts
+++ src/index.ts
@@ -54,10 +54,14 @@ app.post("/razorpay", async (req, res) => {
 app.post("/count", async (req, res) => {
   const dataStructureCount = await User.count({ dataStructures: true });
   const dataScienceCount = await User.count({ dataScience: true });
+  const workshopACount = await User.count({ workshopA: true });
+  const workshopBCount = await User.count({ workshopB: true });
 
   return res.json({
     dataStructureCount: dataStructureCount + 20,
     dataScienceCount: dataScienceCount + 26,
+    workshopACount,
+    workshopBCount,
   });
 });
 
