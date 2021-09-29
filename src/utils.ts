4b9a0256e106b5cd3b000ef0de3937909180df42
Akshay Pandey <codeakshay31@gmail.com>
2021-09-30 01:03:13 +0530

    feat :sendgrid

--- src/utils.ts
+++ src/utils.ts
@@ -2,13 +2,47 @@ import nodemailer from 'nodemailer';
 import env from 'dotenv';
 import mailgun from 'mailgun-js';
 import Promo from './models/Promo';
-const DOMAIN = 'istescmanit.in';
-const mg = mailgun({
-  apiKey: 'dummyKey',
-  domain: DOMAIN,
-});
+import sgMail from '@sendgrid/mail';
+
+// const DOMAIN = 'istescmanit.in';
+// const mg = mailgun({
+//   apiKey: 'dummyKey',
+//   domain: DOMAIN,
+// });
 
 env.config();
+
+// export const sendMail = (
+//   name: string,
+//   email: string,
+//   workshopA: boolean,
+//   workshopB: boolean
+// ) => {
+//   var transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: process.env.SENDER_EMAIL_ID,
+//       pass: process.env.SENDER_EMAIL_PASSWORD,
+//     },
+//   });
+
+//   var mailOptions = {
+//     from: process.env.SENDER_EMAIL_ID,
+//     to: email,
+//     subject: `Confirmation Email for registering to Version Beta 4.0 Workshop`,
+//     text: ` Hi ${name},\n Greetings! \n Thank you for enrolling for Version Beta 4.0 Workshop. Your registration and payment are received successfully. We will be soon back to you with the details about how to attend the workshop.\n\n
+//       Have questions about Version Beta 4.0 workshop or Version Beta 4.0?\nWe would love to help! Just hit reply.\n\nRegards,\nISTE SC MANIT Team`,
+//   };
+
+//   transporter.sendMail(mailOptions, function (error, info) {
+//     if (error) {
+//       return null;
+//     } else {
+//       console.log('Email sent: ' + info.response);
+//       return null;
+//     }
+//   });
+// };
 
 export const sendMail = (
   name: string,
@@ -16,49 +50,43 @@ export const sendMail = (
   workshopA: boolean,
   workshopB: boolean
 ) => {
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.SENDER_EMAIL_ID,
-      pass: process.env.SENDER_EMAIL_PASSWORD,
-    },
-  });
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  var mailOptions = {
-    from: process.env.SENDER_EMAIL_ID,
-    to: email,
+  const msg = {
+    to: email, // Change to your recipient
+    from: process.env.SENDER_EMAIL_ID, // Change to your verified sender
     subject: `Confirmation Email for registering to Version Beta 4.0 Workshop`,
     text: ` Hi ${name},\n Greetings! \n Thank you for enrolling for Version Beta 4.0 Workshop. Your registration and payment are received successfully. We will be soon back to you with the details about how to attend the workshop.\n\n
-      Have questions about Version Beta 4.0 workshop or Version Beta 4.0?\nWe would love to help! Just hit reply.\n\nRegards,\nISTE SC MANIT Team`,
+          Have questions about Version Beta 4.0 workshop or Version Beta 4.0?\nWe would love to help! Just hit reply.\n\nRegards,\nISTE SC MANIT Team`,
   };
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      return null;
-    } else {
-      console.log('Email sent: ' + info.response);
-      return null;
-    }
-  });
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent');
+    })
+    .catch((error: any) => {
+      console.error(error);
+    });
 };
 
-export const sendMail2 = (
-  name: string,
-  email: string,
-  dataScience: boolean,
-  dataStructure: boolean
-) => {
-  const data = {
-    from: 'Excited User <kdevanshsharma23@gmail.com>',
-    to: `dksofficial23@gmail.com `,
-    subject: 'Hello',
-    text: 'Testing some Mailgun awesomness!',
-  };
-  mg.messages().send(data, function (error, body) {
-    console.log(body);
-    console.log(error);
-  });
-};
+// export const sendMail2 = (
+//   name: string,
+//   email: string,
+//   dataScience: boolean,
+//   dataStructure: boolean
+// ) => {
+//   const data = {
+//     from: 'Excited User <kdevanshsharma23@gmail.com>',
+//     to: `dksofficial23@gmail.com `,
+//     subject: 'Hello',
+//     text: 'Testing some Mailgun awesomness!',
+//   };
+//   mg.messages().send(data, function (error, body) {
+//     console.log(body);
+//     console.log(error);
+//   });
+// };
 
 export const updatePromo = async (code: string, id: string) => {
   console.log('testing update', id, code);
