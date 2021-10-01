5b7f639a3d892f79e06bdd4b52d4e93cb56b1af4
Akshay Pandey <codeakshay31@gmail.com>
2021-10-01 16:19:52 +0530

    fix: email content

--- src/utils.ts
+++ src/utils.ts
@@ -53,13 +53,32 @@ export const sendMail = (
 ) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+  const detailA = workshopA
+    ? {
+        name: `Android App Development using Android Studio`,
+        date: `16 - 17 October 2021`,
+      }
+    : { name: ``, date: `` };
+
+  const detailB = workshopB
+    ? {
+        name: `Machine Learning`,
+        date: `19 - 20 October 2021`,
+      }
+    : { name: ``, date: `` };
+
+  const conjuction = workshopA && workshopB ? 'and' : '';
+  const contentA = workshopA
+    ? `Topic : ${detailA.name}\nDate : ${detailA.date}\n`
+    : ``;
+  const contentB = workshopB
+    ? `Topic : ${detailB.name}\nDate : ${detailB.date}\n`
+    : ``;
   const msg = {
     to: email, // Change to your recipient
     from: process.env.SENDER_EMAIL_ID, // Change to your verified sender
     subject: `Confirmation Email for registering to Version Beta 4.0 Workshop`,
-    text: `Hi ${name},\nGreetings! \nThank you for enrolling for Version Beta 4.0 Workshop. Your registration and payment are received successfully. We will be soon back to you with the details about how to attend the workshop.\n
-    Your Payment Id: ${paymentId}\n\n
-    Have questions about Version Beta 4.0 workshop or Version Beta 4.0 Hackathon?\nWe would love to help! Just hit reply.\n\nRegards,\nISTE SC MANIT Team`,
+    text: ` Hi ${name},\nGreetings from ISTE SC MANIT,\nThank you for participating in the fourth edition of Version Beta. We hope you will enjoy learning in the workshop as much as we did in planning for it. ISTE SC MANIT will look forward to seeing you as a participant in this technical conclave.\n\nThis is to inform you that you have been successfully registered for Flair Haven Workshop on ${detailA.name} ${conjuction} ${detailB.name} under Version Beta 4.0.\nThis is your payment ID: ${paymentId}\nYou may note this down for future reference.\n\nDetails of the Workshop\n${contentA}${contentB}\nWe will be soon back to you with the details about how to attend the workshop.If you have any questions about your registration or this event, you can reply to this mail. Let us know if you have any concerns or issues!\n\nRegards,\nISTE SC MANIT Team`,
   };
 
   sgMail
